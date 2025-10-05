da8e5b5e83f2dd5fdfe8127e45aab54b0fc34f3c
Dalip Kumar <114272395+dalip2734@users.noreply.github.com>
2025-10-05 10:32:08 +0000

    Added Actuator

--- GettingStarted/demo/src/main/java/com/example/demo/DemoApplication.java
+++ GettingStarted/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,4 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +11,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-
 	@GetMapping("/")
 	public String welcome() {
 		return "Enter .../hello";
 	}
-
 	@GetMapping("/hello")
 	public String sayHello() {
 		return "Hello, Spring Boot!";
