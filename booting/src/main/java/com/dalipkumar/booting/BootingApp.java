437e2823d2624c937cc2fedbf75a71ef1feab17b
Dalip Kumar <114272395+dalip2734@users.noreply.github.com>
2025-10-09 07:11:47 +0000

    dev & test profile added

--- booting/src/main/java/com/dalipkumar/booting/BootingApp.java
+++ booting/src/main/java/com/dalipkumar/booting/BootingApp.java
@@ -2,10 +2,21 @@ package com.dalipkumar.booting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
 public class BootingApp {
 	public static void main(String[] args) {
 		SpringApplication.run(BootingApp.class, args);
+	}
+
+	@Controller
+	class HelloController{
+		@GetMapping("/hello")
+		public String hello(){
+            return "redirect:/index.html";
+		}
 	}
 }
